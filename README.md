79088e29a2e4f984943b59469b266611af1bbc44
bjornis2 <90688883+bjornis2@users.noreply.github.com>
2022-03-16 11:04:07 +0100

    added login and connections to login

--- README.md
+++
@@ -1,1 +0,0 @@
-# oppgave1
---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+include "db_conn.php";
+
+if(isset($_POST['uname']) && isset($_POST['password'])) {
+
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return data;
+    }
+}
+
+$uname = validate($_POST['uname']);
+$pass = validate($_POST['password']);
+
+if(empty($uname)) {
+    header ("Location: index.php?error=User name is required");
+    exit();
+}
+else if(empty($pass)) {
+    header ("Location: inex.php?error=Password is required");
+    exit();
+}
+
+$sql = "SELECT * FROM users WHERE user_name='$uname' AND password=$pass'";
+
+$result = mysql_query($conn, $sql);
+
+if(mysql_num_rows($result) === 1) {
+    $row = mysql_fetch_assoc($result);
+    if($row['user_name'] === $uname && $row['password'] === $pass) {
+        
+    }
+}
